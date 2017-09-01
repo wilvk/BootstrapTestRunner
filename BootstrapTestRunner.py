a6378f1f79d489ec1fd914b70aac1c348ed6b7bd
Willem van Ketwich <willvk@gmail.com>
2017-09-01 12:56:05 +1000

    var fixes

--- BootstrapTestRunner.py
+++ BootstrapTestRunner.py
@@ -8,35 +8,31 @@ import time
 import unittest
 from xml.sax import saxutils
 
-def to_unicode(s):
+def to_unicode(raw_string):
     try:
-        return unicode(s)
+        return unicode(raw_string)
     except UnicodeDecodeError:
-        return s.decode('unicode_escape')
+        return raw_string.decode('unicode_escape')
 
 class OutputRedirector(object):
-    def __init__(self, fp):
-        self.fp = fp
+    def __init__(self, file_pointer):
+        self.file_pointer = file_pointer
 
-    def write(self, s):
-        self.fp.write(to_unicode(s))
+    def write(self, raw_string):
+        self.file_pointer.write(to_unicode(raw_string))
 
     def writelines(self, lines):
         lines = map(to_unicode, lines)
-        self.fp.writelines(lines)
+        self.file_pointer.writelines(lines)
 
     def flush(self):
-        self.fp.flush()
+        self.file_pointer.flush()
 
 stdout_redirector = OutputRedirector(sys.stdout)
 stderr_redirector = OutputRedirector(sys.stderr)
 
 class Template_mixin(object):
-    STATUS = {
-        0: 'pass',
-        1: 'fail',
-        2: 'error',
-    }
+    STATUS = { 0: 'pass', 1: 'fail', 2: 'error'}
     DEFAULT_TITLE = 'Unit Test Report'
     DEFAULT_DESCRIPTION = ''
     HTML_TMPL = r"""
@@ -50,20 +46,9 @@ class Template_mixin(object):
     %(stylesheet)s
 </head>
 <body>
-    <script language="javascript" type="text/javascript">
-
-    function html_escape(s) {
-        s = s.replace(/&/g,'&amp;');
-        s = s.replace(/</g,'&lt;');
-        s = s.replace(/>/g,'&gt;');
-        return s;
-    }
-    </script>
-
     %(heading)s
     %(report)s
     %(ending)s
-
 </body>
 </html>
 """
@@ -74,20 +59,19 @@ class Template_mixin(object):
 """
     HEADING_TMPL = """
 <div class="container-fluid">
-    <div class='heading'>
+    <div>
         <h1>%(title)s</h1>
         %(parameters)s
-        <p class='description'>%(description)s</p>
+        <p><strong>Test Description: </strong>%(description)s</p>
     </div>
 """
     HEADING_ATTRIBUTE_TMPL = """
     <p><strong>%(name)s:</strong> %(value)s</p>
 """
     REPORT_TMPL = """
-    <p id='show_detail_line'>Show
-        <a href='javascript:showCase(0)'>Summary</a>
-        <a href='javascript:showCase(1)'>Failed</a>
-        <a href='javascript:showCase(2)'>All</a>
+    <p id='show_detail_line'>
+        <button type="button" class="btn btn-info" data-toggle="collapse" data-target="%(all_test_classes)s">Toggle All</button></td>
+        <button type="button" class="btn btn-info" data-toggle="collapse" data-target="%(all_passed_test_classes)s">Toggle Failed</button></td>
     </p>
     <table class="table table-striped">
         <colgroup>
@@ -100,21 +84,21 @@ class Template_mixin(object):
         </colgroup>
         <thead class="thead-inverse">
             <tr id='header_row'>
-                <th class="col-md-3">Test Group/Test case</th>
-                <th class="col-md-2">Count</th>
-                <th class="col-md-2">Pass</th>
-                <th class="col-md-2">Fail</th>
-                <th class="col-md-2">Error</th>
+                <th class="col-md-7">Test Group/Test case</th>
+                <th class="col-md-1">Count</th>
+                <th class="col-md-1">Pass</th>
+                <th class="col-md-1">Fail</th>
+                <th class="col-md-1">Error</th>
                 <th class="col-md-1">View</th>
             </tr>
         </thead>
         %(test_list)s
         <tr id='total_row'>
-            <th class="col-md-3">Total</th>
-            <th class="col-md-2">%(count)s</th>
-            <th class="col-md-2">%(passed)s</th>
-            <th class="col-md-2">%(fail)s</th>
-            <th class="col-md-2">%(error)s</th>
+            <th class="col-md-7">Total</th>
+            <th class="col-md-1">%(count)s</th>
+            <th class="col-md-1">%(passed)s</th>
+            <th class="col-md-1">%(fail)s</th>
+            <th class="col-md-1">%(error)s</th>
             <th class="col-md-1">&nbsp;</th>
         </tr>
     </table>
@@ -146,7 +130,7 @@ class Template_mixin(object):
 """
     REPORT_TEST_NO_OUTPUT_TMPL = r"""
     <tr id='tr_%(tid)s' class='%(class_name)s'>
-        <td class='%(style)s'><div class='testcase'>%(desc)s</div></td>
+        <td class='%(style)s'><div>%(desc)s</div></td>
         <td colspan='5' align='center'>%(status)s</td>
     </tr>
 """
@@ -161,6 +145,7 @@ class Template_mixin(object):
 TestResult = unittest.TestResult
 
 class _TestResult(TestResult):
+
     def __init__(self, verbosity=1):
         TestResult.__init__(self)
         self.outputBuffer = StringIO.StringIO()
@@ -174,8 +159,8 @@ class _TestResult(TestResult):
 
     def startTest(self, test):
         TestResult.startTest(self, test)
-        stdout_redirector.fp = self.outputBuffer
-        stderr_redirector.fp = self.outputBuffer
+        stdout_redirector.file_pointer = self.outputBuffer
+        stderr_redirector.file_pointer = self.outputBuffer
         self.stdout0 = sys.stdout
         self.stderr0 = sys.stderr
         sys.stdout = stdout_redirector
@@ -257,15 +242,15 @@ class BootstrapTestRunner(Template_mixin):
         rmap = {}
         classes = []
         for n, t, o, e in result_list:
-            cls = t.__class__
-            if not rmap.has_key(cls):
-                rmap[cls] = []
-                classes.append(cls)
-            rmap[cls].append((n,t,o,e))
-        r = [(cls, rmap[cls]) for cls in classes]
-        return r
+            test_class = t.__class__
+            if not rmap.has_key(test_class):
+                rmap[test_class] = []
+                classes.append(test_class)
+            rmap[test_class].append((n,t,o,e))
+        results = [(test_class, rmap[test_class]) for test_class in classes]
+        return results
 
-    def getReportAttributes(self, result):
+    def get_report_attributes(self, result):
         startTime = str(self.startTime)[:19]
         duration = str(self.stopTime - self.startTime)
         status = []
@@ -279,27 +264,17 @@ class BootstrapTestRunner(Template_mixin):
             status = ' '.join(status)
         else:
             status = 'none'
-        return [
-            ('Start Time', startTime),
-            ('Duration', duration),
-            ('Status', status),
-        ]
+        return [ ('Start Time', startTime), ('Duration', duration), ('Status', status) ]
 
     def generateReport(self, test, result):
-        report_attrs = self.getReportAttributes(result)
+        report_attrs = self.get_report_attributes(result)
         generator = 'HTMLTestRunner %s' % __version__
         stylesheet = self._generate_stylesheet()
         heading = self._generate_heading(report_attrs)
         report = self._generate_report(result)
         ending = self._generate_ending()
-        output = self.HTML_TMPL % dict(
-            title = saxutils.escape(self.title),
-            generator = generator,
-            stylesheet = stylesheet,
-            heading = heading,
-            report = report,
-            ending = ending,
-        )
+        output = self.HTML_TMPL % dict(title = saxutils.escape(self.title), generator = generator, stylesheet = stylesheet, heading = heading, report = report,
+                                       ending = ending)
         self.stream.write(output.encode('utf8'))
 
     def _generate_stylesheet(self):
@@ -308,79 +283,96 @@ class BootstrapTestRunner(Template_mixin):
     def _generate_heading(self, report_attrs):
         a_lines = []
         for name, value in report_attrs:
-            line = self.HEADING_ATTRIBUTE_TMPL % dict(
-                    name = saxutils.escape(name),
-                    value = saxutils.escape(value),
-                )
+            line = self.HEADING_ATTRIBUTE_TMPL % dict(name = saxutils.escape(name), value = saxutils.escape(value))
             a_lines.append(line)
-        heading = self.HEADING_TMPL % dict(
-            title = saxutils.escape(self.title),
-            parameters = ''.join(a_lines),
-            description = saxutils.escape(self.description),
-        )
+        heading = self.HEADING_TMPL % dict(title = saxutils.escape(self.title), parameters = ''.join(a_lines), description = saxutils.escape(self.description))
         return heading
 
+    def get_test_numbers_from_class_results(self, class_results):
+        no_passed = no_failed = no_errored = 0
+        for status_id, t, o, e in class_results:
+            if status_id == 0:
+                no_passed += 1
+            elif status_id == 1:
+                no_failed += 1
+            else: no_errored += 1
+        return no_passed, no_failed, no_errored
+
+    def get_descriptions_from_test_class(self, test_class):
+        if test_class.__module__ == '__main__':
+            class_name = test_class.__name__
+        else:
+            class_name = '%s.%s' % (test_class.__module__, test_class.__name__)
+        if test_class.__doc__:
+            class_docstring = test_class.__doc__.split("\n")[0]
+        else:
+            class_docstring = ''
+        if class_docstring:
+            description = '%s: %s' % (class_name, class_docstring)
+        else:
+            description = class_name
+        return class_name, class_docstring, description
+
     def _generate_report(self, result):
-        rows = []
+        test_rows = []
+        all_passed_tests = []
+        all_tests = []
         sorted_result = self.sortResult(result.result)
-        for class_id, (cls, cls_results) in enumerate(sorted_result):
-            no_passed = no_failed = no_errored = 0
-            for no_passed, t, o, e in cls_results:
-                if no_passed == 0:
-                    no_passed += 1
-                elif no_passed == 1:
-                    no_failed += 1
-                else: no_errored += 1
-
-            if cls.__module__ == '__main__':
-                class_name = cls.__name__
-            else:
-                class_name = '%s.%s' % (cls.__module__, cls.__name__)
-            class_docstring = cls.__doc__ and cls.__doc__.split("\n")[0] or ''
-            desc = class_docstring and '%s: %s' % (class_name, class_docstring) or class_name
-
+        for class_id, (test_class, class_results) in enumerate(sorted_result):
+            (no_passed, no_failed, no_errored) = self.get_test_numbers_from_class_results(class_results)
+            (class_name, class_docstring, description) = self.get_descriptions_from_test_class(test_class)
             passed_tests = []
+            class_tests = []
+            for test_id, (status_id, t, o, e) in enumerate(class_results):
+                test_string = '#tr_' + self._generate_tid_string(status_id, test_id, class_id)
+                if status_id == 0:
+                    passed_tests.append(test_string)
+                all_tests.append(test_string)
 
-            for test_id, (no_passed, t, o, e) in enumerate(cls_results):
-                if no_passed == 0:
-                    passed_tests.append('#tr_' + self._generate_tid_string(no_passed, test_id, class_id))
+            all_passed_tests.extend(passed_tests)
+            if no_errored > 0:
+                style_class = 'warning'
+            elif no_failed > 0:
+                style_class = 'danger'
+            else:
+                style_class = 'success'
 
-            row = self.REPORT_CLASS_TMPL % dict(
-                style = no_errored > 0 and 'danger' or no_failed > 0 and 'danger' or 'success',
-                desc = desc,
-                count = no_passed + no_failed + no_errored,
-                passed = no_passed,
-                fail = no_failed,
-                error = no_errored,
-                class_id = 'c%s' % (class_id + 1),
-                tids = ','.join(passed_tests),
-            )
-            rows.append(row)
+            test_count = no_passed + no_failed + no_errored
+            class_id_string = 'c%s' % (class_id + 1)
+            test_ids_string = ','.join(passed_tests)
+            test_row = self.REPORT_CLASS_TMPL % dict(style = style_class, desc = description, count = test_count, passed = no_passed, fail = no_failed,
+                                                     error = no_errored, class_id = class_id_string, tids = test_ids_string)
 
-            for test_id, (no_passed, test_name, output_text, error_text) in enumerate(cls_results):
-                self._generate_report_test(rows, class_id, test_id, no_passed, test_name, output_text, error_text)
+            test_rows.append(test_row)
 
-        report = self.REPORT_TMPL % dict(
-            test_list = ''.join(rows),
-            count = str(result.success_count + result.failure_count + result.error_count),
-            passed = str(result.success_count),
-            fail = str(result.failure_count),
-            error = str(result.error_count),
-        )
+            for test_id, (status_id, test_name, output_text, error_text) in enumerate(class_results):
+                self._generate_report_test(test_rows, class_id, test_id, status_id, test_name, output_text, error_text)
+
+        total_test_count = result.success_count + result.failure_count + result.error_count
+        all_passed_tests_string = ','.join(all_passed_tests)
+        all_tests_string = ','.join(all_tests)
+        report = self.REPORT_TMPL % dict(test_list = ''.join(test_rows), count = str(total_test_count), passed = str(result.success_count),
+                                         fail = str(result.failure_count), error = str(result.error_count), all_passed_test_classes = all_passed_tests_string,
+                                         all_test_classes = all_tests_string)
         return report
 
-    def _generate_tid_string(self, no_fails, test_id, class_id):
-        prefix = 'p' if no_fails == 0 else 'f'
+    def _generate_tid_string(self, status_id, test_id, class_id):
+        prefix = 'p' if status_id == 0 else 'f'
         return '%s-t%s-%s' % (prefix, class_id + 1, test_id + 1)
 
-    def _generate_report_test(self, rows, class_id, test_id, no_passed, test_name, test_output, exception_output):
+    def get_report_test_variables(self, class_id, test_id, status_id, test_name, test_output, exception_output):
         has_output = bool(test_output or exception_output)
-        test_id_string = self._generate_tid_string(no_passed, test_id, class_id)
+        test_id_string = self._generate_tid_string(status_id, test_id, class_id)
         test_name_short = test_name.id().split('.')[-1]
         short_description = test_name.shortDescription() or ''
-        description = short_description and ('%s: %s' % (test_name_short, short_description)) or test_name_short
-        template = has_output and self.REPORT_TEST_WITH_OUTPUT_TMPL or self.REPORT_TEST_NO_OUTPUT_TMPL
-
+        if short_description:
+            description = ('%s: %s' % (test_name_short, short_description))
+        else:
+            description = test_name_short
+        if has_output:
+            template = self.REPORT_TEST_WITH_OUTPUT_TMPL
+        else:
+            template = self.REPORT_TEST_NO_OUTPUT_TMPL
         if isinstance(test_output, str):
             unicode_test_output = test_output.decode('latin-1')
         else:
@@ -389,22 +381,26 @@ class BootstrapTestRunner(Template_mixin):
             unicode_exception_output = exception_output.decode('latin-1')
         else:
             unicode_exception_output = exception_output
+        script = self.REPORT_TEST_OUTPUT_TMPL % dict(id = test_id_string, output = saxutils.escape( unicode_test_output + unicode_exception_output ))
+        if status_id == 0:
+            class_name = 'collapse'
+        else:
+            class_name = 'none'
+        if status_id == 2:
+            text_style = 'text-danger'
+        elif status_id == 1:
+            text_style = 'text-danger'
+        else:
+            text_style = 'none'
+        class_id_string = 'c%s' % ( class_id + 1 )
+        return template, test_id_string, class_name, text_style, description, script, class_id_string, has_output
 
-        script = self.REPORT_TEST_OUTPUT_TMPL % dict(
-            id = test_id_string,
-            output = saxutils.escape( unicode_test_output + unicode_exception_output ),
-        )
-
-        row = template % dict(
-            tid = test_id_string,
-            class_name = (no_passed == 0 and 'collapse' or 'none'),
-            style = no_passed == 2 and 'text-danger' or (no_passed == 1 and 'text-danger' or 'none'),
-            desc = description,
-            script = script,
-            cid = 'c%s' % ( class_id + 1 ),
-            status = self.STATUS[no_passed],
-        )
-        rows.append(row)
+    def _generate_report_test(self, test_rows, class_id, test_id, status_id, test_name, test_output, exception_output):
+        (template, test_id_string, class_name, text_style, description, script, class_id_string, has_output) = self.get_report_test_variables(
+            class_id, test_id, status_id, test_name, test_output, exception_output)
+        test_row = template % dict(tid = test_id_string, class_name = class_name, style = text_style, desc = description, script = script,
+                                   cid = class_id_string, status = self.STATUS[status_id])
+        test_rows.append(test_row)
         if not has_output:
             return
 
